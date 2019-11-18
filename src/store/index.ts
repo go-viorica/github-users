23665060b2af9bf12cbf0ae4b709c1b863687775
Viorica Gonta <vg@bidmotion.com>
2019-11-18 08:39:29 +0200

    Setup store actions & state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,47 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import { getUsers, getUserRepositories } from '@/http/axios';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    users: [],
+    mapUserIdToRepositories: {} as { [key: string]: any[] },
+  },
+  mutations: {
+    SET_USERS(state, users) {
+      state.users = users;
+    },
+    SET_USER_REPOSITORY(state, { repositories, userId }) {
+      state.mapUserIdToRepositories = {
+        ...state.mapUserIdToRepositories,
+        [userId]: repositories,
+      };
+    },
+  },
+  actions: {
+    fetchAllUsers({ commit }) {
+      getUsers()
+        .then(users => {
+          commit('SET_USERS', users);
+        })
+        .catch(error => {
+          console.log('getUsers ERROR: ', error);
+        });
+    },
+    fetchUserRepositories({ commit }, { userId }) {
+      if (this.state.mapUserIdToRepositories[userId]) {
+        return;
+      }
+      getUserRepositories(userId)
+        .then(repositories => {
+          commit('SET_USER_REPOSITORY', { repositories, userId });
+        })
+        .catch(error => {
+          console.log('getUserRepositories ERROR: ', error);
+        });
+    },
+  },
 });
